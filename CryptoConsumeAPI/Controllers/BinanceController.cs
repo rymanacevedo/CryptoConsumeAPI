62401b6229a3a7b2d3a52c8998f77a4f9961936b
Ryan Acevedo <rymanacevedo@gmail.com>
2022-01-21 16:50:15 -0700

    removed unused depdencies

--- CryptoConsumeAPI/Controllers/BinanceController.cs
+++ CryptoConsumeAPI/Controllers/BinanceController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using CryptoConsumeAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -43,7 +40,6 @@ namespace CryptoConsumeAPI.Controllers
             {
                 //https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT
                 builder.Append($"/ticker/price?symbol={pair}");
-                Console.WriteLine(builder.ToString());
                 result = await HTTPClientWrapper<dynamic>.Get(builder.ToString());
             }
             return result;
--- CryptoConsumeAPI/Controllers/KuCoinController.cs
+++ CryptoConsumeAPI/Controllers/KuCoinController.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.Text.Json;
 using CryptoConsumeAPI.Models;
 using System.Text;

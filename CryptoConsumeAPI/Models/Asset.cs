8bf7fa5a5ebea349bcbe2632b52cae0bee0c912e
Ryan Acevedo <racevedo@amplifire.com>
2022-07-15 14:29:06 -0600

    updated Model

--- CryptoConsumeAPI/Models/Asset.cs
+++ CryptoConsumeAPI/Models/Asset.cs
@@ -3,7 +3,7 @@ namespace CryptoConsumeAPI.Models
 {
     public class Asset
     {
-        public Asset(string id, string name, string description, decimal price, decimal quantity, string exchange, string currency, string api)
+        public Asset(string id, string name, object description, decimal price, decimal quantity, string exchange, string currency, string api)
         {
             Id = id;
             Name = name;
@@ -16,8 +16,7 @@ namespace CryptoConsumeAPI.Models
         }
         public string Id { get; }
         public string Name { get; }
-        public string Description { get; }
-
+        public object Description { get; }
         public decimal Price { get; }
         public decimal Quantity { get; }
         public string Exchange { get; }
